deb088e8d05a00bce804dc6231750a720714b31e
viralpatel <viralpatel.net@gmail.com>
2011-02-08 03:19:44 +0530

    source added 	new file:   src/app/controllers/Application.java 	new file:   src/app/controllers/Auth.gaejava 	new file:   src/app/controllers/Auth.java 	new file:   src/app/controllers/Auth.nongaejava 	new file:   src/app/controllers/Pages.java 	new file:   src/app/controllers/Profile.java 	new file:   src/app/ext/Extensions.java 	new file:   src/app/models/Link.java 	new file:   src/app/models/LinkTag.java 	new file:   src/app/models/Tag.java 	new file:   src/app/models/User.java 	new file:   src/app/views/Application/index.html 	new file:   src/app/views/Pages/about.html 	new file:   src/app/views/Pages/tools.html 	new file:   src/app/views/Profile/edit.html 	new file:   src/app/views/Profile/editlink.html 	new file:   src/app/views/Profile/index.html 	new file:   src/app/views/errors/404.html 	new file:   src/app/views/errors/500.html new file:   src/app/views/main.html 	new file:   src/conf/application.conf new file:   src/conf/gae_application.conf 	new file:   src/conf/messages 	new file:   src/conf/nongae_application.conf 	new file:   src/conf/routes 	new file:   src/lib/jericho-html-3.1.jar 	new file:   src/lib/play-siena.jar 	new file:   src/lib/siena-0.7.6.jar 	new file:   src/public/images/Thumbs.db 	new file:   src/public/images/eazybookmarklet-example-screenshot.png 	new file:   src/public/images/favicon.ico 	new file:   src/public/images/favicon.png 	new file:   src/public/stylesheets/main.css 	new file:   src/test/Application.test.html 	new file:   src/test/ApplicationTest.java 	new file:   src/test/BasicTest.java 	new file:   src/test/data.yml

---
+++ src/app/models/User.java
@@ -0,0 +1,43 @@
+package models;
+
+import siena.*;
+import java.util.*;
+
+public class User extends Model {
+
+    @Id
+    public Long id;
+
+    public String email;
+	public String name;
+	public Date created;
+	public Date modified;
+
+    /*@Index("link_index")
+    public Link link;
+	*/
+
+    static Query<User> all() {
+        return Model.all(User.class);
+    }
+
+    public static User findById(Long id) {
+        return all().filter("id", id).get();
+    }
+
+    public static User findByEmail(String email) {
+        return all().filter("email", email).get();
+    }
+	public User() {
+		super();
+	}
+    public User(String email) {
+		this.email = email;
+    }
+
+    public String toString() {
+        return email;
+    }
+
+}
+
